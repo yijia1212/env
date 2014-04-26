87028008ebaec24a76388ddc7be85c8b0f0ef736
Yijia <guyijia@gmail.com>
2014-04-26 12:57:45 -0400

    .emacs.d

---
+++ .emacs.d/custom_base.el
@@ -0,0 +1,33 @@
+(provide 'custom_base)
+;;Set default browser
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "chromium-browser")
+;;Set spell check
+(setq-default ispell-program-name "aspell") 
+;;Hide the menu bar
+;;(menu-bar-mode -1)
+;;No splash screen
+(setq inhibit-splash-screen t)
+;;Disable toolbar
+(tool-bar-mode -1)
+;;Disable scrollbar
+(scroll-bar-mode -1)
+
+(mapc (lambda (mode)
+      (add-hook 'LaTeX-mode-hook mode))
+      (list 'auto-fill-mode
+            'LaTeX-math-mode
+            'turn-on-reftex
+            'linum-mode))
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (setq TeX-auto-untabify t     ; remove all tabs before saving
+                  TeX-show-compilation t) ; display compilation windows
+            (TeX-global-PDF-mode t)       ; PDF mode enable, not plain
+            (setq TeX-save-query nil)
+            (imenu-add-menubar-index)
+            (define-key LaTeX-mode-map (kbd "TAB") 'TeX-complete-symbol)))
+
+
+
+
